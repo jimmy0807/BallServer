9a3d7597095453430c68b212558e417b04670b4b
jimmy <>
2018-12-05 18:18:04 +0800

    准备修正位置

--- h5/BallAction/BallActionManager.js
+++ h5/BallAction/BallActionManager.js
@@ -25,10 +25,56 @@ BallAction.prototype.moveToTargetPostion = function(ball)
     count = 10
     catchUp = ball.catchUp
    
-    catchUp.remoteX = parseFloat(message.pos.split(",")[0])
-    catchUp.remoteY = parseFloat(message.pos.split(",")[1])
+    catchUp.x = parseFloat(message.pos.split(",")[0])
+    catchUp.y = parseFloat(message.pos.split(",")[1])
     catchUp.xflag = message.pos.split(",")[2] == "1" ? 1 : -1;
-    catchUp.yflag = message.pos.split(",")[3] == "1" ? 1 : -1; 
+    catchUp.yflag = message.pos.split(",")[3] == "1" ? 1 : -1;
+    catchUp.speedX = ball.speedX
+    catchUp.speedY = ball.speedY
+    catchUp.offsetWidth = ball.offsetWidth
+    catchUp.offsetHeight = ball.offsetHeight
+
+    caculateNextPostion(ball, count)
+}
+
+function caculateNextPostion(ball, count)
+{
+    catchUp = ball.catchUp
+
+    for ( var i = 0; i < count; i++ )
+    {
+        if (catchUp.yflag == 1) {
+            //小球向下移动
+            catchUp.y += catchUp.speedY;
+            if (catchUp.y >= 800 - catchUp.offsetWidth) {
+                catchUp.y = 800 - catchUp.offsetWidth;
+                catchUp.yflag = -1;
+            }
+        } else if (catchUp.yflag == -1) {
+            //小球向上移动
+            catchUp.y -= catchUp.speedY;
+            if (catchUp.y <= 0) {
+                catchUp.y = 0;
+                catchUp.yflag = 1;
+            }
+        }
+    
+        if ( catchUp.xflag == 1 ) {
+            //小球向右移动
+            catchUp.x += catchUp.speedX;
+            if (catchUp.x >= 1300 - catchUp.offsetHeight) {
+                catchUp.x = 1300 - catchUp.offsetHeight;
+                catchUp.xflag = -1;
+            }
+        } else if (ball.xflag == -1) {
+            //小球向左移动
+            catchUp.x -= catchUp.speedX;
+            if (catchUp.x <= 0) {
+                catchUp.x = 0;
+                catchUp.xflag = 1;
+            }
+        }
+    }
 }
 
 BallAction.prototype.justMove = function(ball)
